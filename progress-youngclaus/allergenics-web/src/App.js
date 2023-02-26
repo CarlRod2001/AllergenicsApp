b95ec0ea1b91f82dc5d0d5fba980a20559d463ce
Chris Youngclaus <cyoungcl@stevens.edu>
2023-02-26 00:51:31 -0500

    Website functionality and basic customization

--- progress-youngclaus/allergenics-web/src/App.js
+++ progress-youngclaus/allergenics-web/src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
+import logo from "./allergenicsLogo.png";
 import './App.css';
+
+// npm start
+    // localhost:3000
+// ctrl c
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" src={logo} alt="logo" />
         <p>
           Allergenics Website Development Server
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Allergenics"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Track our progress!
         </a>
       </header>
     </div>
