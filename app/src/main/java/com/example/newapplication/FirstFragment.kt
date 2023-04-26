7802d1cdece03d9e355f5dff3e2c84d78d5ea2c7
Carlitos Rodriguez <crodrig7@stevens.edu>
2023-04-26 00:32:01 -0400

    Map is still Broken

--- app/src/main/java/com/example/newapplication/FirstFragment.kt
+++ app/src/main/java/com/example/newapplication/FirstFragment.kt
@@ -1,5 +1,4 @@
 package com.example.newapplication
-
 import android.R
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -9,58 +8,125 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import com.example.newapplication.databinding.FragmentFirstBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-
+import com.google.android.gms.maps.model.MarkerOptions
 
 class FirstFragment : Fragment(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
     private lateinit var binding: FragmentFirstBinding
     private val LOCATION_PERMISSION_REQUEST = 1
+
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentFirstBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = childFragmentManager
+            .findFragmentById(com.example.newapplication.R.id.gmap) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(com.example.newapplication.R.layout.fragment_first, container, false)
     }
+
     private fun getLocationAccess() {
-        if (ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             map.isMyLocationEnabled = true
         }
-        else
-        {
+        else {
             ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST)
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode:Int, permissions:Array<String>, grantResults:IntArray) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST) {
-            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+    override fun onRequestPermissionsResult(requestCode:Int, permissions:Array<String>, grantResults:IntArray)
+    {
+        if (requestCode == LOCATION_PERMISSION_REQUEST)
+        {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED))
+            {
                 map.isMyLocationEnabled = true
             }
             else {
-                Toast.makeText(requireActivity(), "User has not granted location permission", Toast.LENGTH_LONG).show()
-                finish()
+                Toast.makeText(requireContext(), "User has not granted location permission", Toast.LENGTH_LONG).show()
             }
         }
     }
-    override fun onMapReady(gmap: GoogleMap) {
-        map = gmap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        //Toast.makeText(this, "test dialog3", Toast.LENGTH_LONG).show()
+        //Toast.makeText(getApplicationContext(), " Hello World", Toast.LENGTH_SHORT).show();
         getLocationAccess()
+
+        // Add a marker in Sydney and move the camera
+        //val sydney = LatLng(-34.0, 151.0)
+        // Add a marker in Hoboken and move the camera
+
+        /*var zommLevel = 15f
+         val hoboken = LatLng(40.745255, -74.034775)
+         map.addMarker(
+             MarkerOptions().position(hoboken).title("Hoboken, NJ")
+                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+         )
+         map.moveCamera(CameraUpdateFactory.newLatLngZoom(hoboken, zommLevel))
+         */
     }
 
-
 }
+//com.example.newapplication.R.layout.fragment_first
+//com.example.newapplication.R.id.gmap
+
+/*
+private lateinit var map: GoogleMap
+    private lateinit var binding: FragmentFirstBinding
+    private val LOCATION_PERMISSION_REQUEST = 1
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+        return (binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //replaced supportFragmentManager with childFragmentManager
+
+
+        /*
+        val mapFragment = childFragmentManager
+            .findFragmentById(com.example.newapplication.R.id.gmap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return inflater.inflate(com.example.newapplication.R.layout.fragment_first, container, false)
+        */
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        //Toast.makeText(this, "test dialog3", Toast.LENGTH_LONG).show()
+        //Toast.makeText(getApplicationContext(), " Hello World", Toast.LENGTH_SHORT).show();
+        getLocationAccess()
+
+        // Add a marker in Sydney and move the camera
+        //val sydney = LatLng(-34.0, 151.0)
+        // Add a marker in Hoboken and move the camera
+
+        /*var zommLevel = 15f
+         val hoboken = LatLng(40.745255, -74.034775)
+         map.addMarker(
+             MarkerOptions().position(hoboken).title("Hoboken, NJ")
+                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+         )
+         map.moveCamera(CameraUpdateFactory.newLatLngZoom(hoboken, zommLevel))
+         */
+    }
+
+*/
